43c786bfcfe0102424d4bfd25af124bd984b0ce0
HpzGit <3213796489@qq.com>
2025-04-21 09:13:38 +0800

    2

---
+++ 移植SGI STL库内存池手写/CommonConnectionPool.cpp
@@ -0,0 +1,76 @@
+#include "CommonConnectionPool.h"
+//构造函数和析构函数
+CommonConnectionPool::CommonConnectionPool()
+{
+	if (!loadConfigFile()) {
+		return;
+	}
+	/*
+	创建初始数量的连接
+	*/
+	for (int i = 0; i < _initSize; i++) {
+		Connection* p = new Connection();
+		p->connect(_ip, _port, _username, _password, _dbname);
+		_connectionQue.push(p);
+		_connectionCnt++;
+	}
+	//启动一个新的线程，作为连接的生产者
+}
+
+CommonConnectionPool::~CommonConnectionPool()
+{
+}
+//线程安全的懒汉单例模式
+CommonConnectionPool* CommonConnectionPool::getConnectionPool() {
+	static CommonConnectionPool pool;//静态局部变量，编译器自动lock和unlock，线程安全
+	return &pool;
+} 
+bool CommonConnectionPool::loadConfigFile() {
+	FILE* pf = fopen("mysql.ini", "r");
+	if (pf == nullptr) {
+		LOG("mysql.ini not found");
+		return false;
+	}
+	while (!feof(pf)) {
+		char line[1024] = { 0 };
+		fgets(line, 1024, pf);
+		string str = line;
+		int idx = str.find("=",0);
+		if (idx == -1) {//空行或者注释行
+			continue;
+		}
+		//解析key-value
+		//password = root\n
+		int endIdx = str.find("\n", idx);
+		string key = str.substr(0, idx);
+		string value = str.substr(idx + 1, endIdx - idx - 1);
+		if (key == "ip") {
+			_ip = value;
+		}
+		else if (key == "port") {
+			_port = atoi(value.c_str());
+		}
+		else if (key == "dbname") {
+			_dbname = value;
+		}
+		else if (key == "username") {
+			_username = value;
+		}
+		else if (key == "password") {
+			_password = value;
+		}
+		else if (key == "initSize") {
+			_initSize = atoi(value.c_str());
+		}
+		else if (key == "maxSize") {
+			_maxSize = atoi(value.c_str());
+		}
+		else if (key == "maxIdleTime") {
+			_maxIdleTime = atoi(value.c_str());
+		}
+		else if (key == "connectionTimeout") {
+			_connectionTimeout = atoi(value.c_str());
+		}
+	}
+	return true;
+}
--- 移植SGI STL库内存池手写/CommonConnectionPool.h
+++ 移植SGI STL库内存池手写/CommonConnectionPool.h
@@ -1,4 +1,38 @@
 #pragma once
 /*
 连接池功能模块
+//单例模式
 */
+#include <list>
+#include <mutex>
+#include <condition_variable>
+#include<string>
+#include<queue>
+#include<atomic>
+#include<thread>
+#include<iostream>
+#include"Connection.h"
+using namespace std;
+class CommonConnectionPool
+{
+public:
+	//获取连接池实例
+	static CommonConnectionPool* getConnectionPool();
+private:
+	//构造函数私有化
+	CommonConnectionPool();
+	bool loadConfigFile();//加载配置文件
+private:
+	string _ip;//数据库IP地址
+	unsigned short _port;//数据库端口号
+	string _username;//数据库用户名
+	string _password;//数据库密码
+	string _dbname;//数据库名称
+	int _initSize;//连接池初始化大小
+	int _maxSize;//连接池最大大小
+	int _maxIdleTime;//连接最大空闲时间
+	int _connectionTimeout;//连接超时时间
+	atomic_int _connectionCnt;//连接池当前连接数
+	queue<Connection*> _connectionQue;//连接池
+	mutex _queueMutex;//连接池互斥锁
+};
